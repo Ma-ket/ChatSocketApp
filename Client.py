a42ec007d221abd37176f026ef3bdff1904dc97d
MakotoHamada <makoto.market0113@gmail.com>
2024-01-05 19:36:32 +0900

    ChatSocketApp begin

---
+++ Client.py
@@ -0,0 +1,39 @@
+import sys
+import socket
+import json
+from Packet import Packet
+
+class Client:
+    self._pckt = None
+    self._sock = None
+
+    def __init__(self):
+        self._pckt = Packet("client", None)
+        self._sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    def input_username(pckt):
+        """ user名の入力 """
+        user_name = input(f"user name: ")
+        print(user_name)
+        return user_name
+
+    def create_packet(self, username, registered=False, comment=None):
+        """ パケットの作成 """
+        self._pckt.set_packetdata(username, registered, comment)
+        data = self._pckt.get_packetdata()
+        return data
+
+    def send_packet(self, data, addr):
+        """ パケットの送信 """
+        data = json.dumps(data)
+        send_len = self._sock.sendto(data.encode('utf-8'), addr)
+        return send_len
+
+    def close(commnet=""):
+        """ 終了処理 """
+        self._sock.close()
+        sys.exit(commnet)
+
+
+
+
---
+++ Packet.py
@@ -0,0 +1,25 @@
+class Packet:
+    _app = None
+    _name = None
+    _type = None
+    _registered = False
+    _comment = None
+
+    def __init__(self, app, put_type=None):
+        self._app = app
+        self._type = put_type
+
+    def set_packetdata(self, name=None, registered=False, comment=None):
+        self._name = self._name if (name is None) else name
+        self._registered = registered
+        self._comment = comment
+
+    def get_packetdata(self):
+        data = {
+            "app" : self._app,
+            "user_name" :self._name,
+            "type" : self._type,
+            "registered" : self._registered,
+            "comment" : self._comment
+        }
+        return data
