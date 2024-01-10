e4069d90be8d5bf93fb042b7730e3f504950a1d1
MakotoHamada <makoto.market0113@gmail.com>
2024-01-10 23:15:53 +0900

    関数の概要を追加

--- User_Addresses.py
+++ User_Addresses.py
@@ -2,15 +2,18 @@ import sys
 
 class User_Addresses:
     def __init__(self):
+        """ コンストラクタ """
         self.user_dict = dict()
         pass
 
     def name_exists(self, name):
+        """ 辞書に名前があるかどうか """
         if (name in self.user_dict):
             return True
         return False
 
     def set_addr(self, name, type_input=None, type_output=[]):
+        """ addrを登録する """
         tmp_inout = self.user_dict[name]
         tmp_in, tmp_outs = tmp_inout
         if (type_input is not None):
@@ -21,6 +24,7 @@ class User_Addresses:
             self.user_dict[name] = (tmp_in, tmp_outs)
 
     def get_addr(self, name, put_type=None):
+        """ addrを返す """
         if (put_type is None):
             pass
         elif (put_type == "input"):
@@ -32,9 +36,11 @@ class User_Addresses:
         pass
 
     def put_type_in(self, name):
+        """ 入力app側の処理 """
         pass
 
     def put_type_out(self, name):
+        """ 出力app側の処理 """
         pass
 
 
