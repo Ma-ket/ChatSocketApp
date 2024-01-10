c63ce46d614f1f468d31030c4ae8b346aa5ca3b8
MakotoHamada <makoto.market0113@gmail.com>
2024-01-10 23:46:59 +0900

    User_Addresses.pyの完成

--- User_Addresses.py
+++ User_Addresses.py
@@ -12,6 +12,12 @@ class User_Addresses:
             return True
         return False
 
+    def create_user_dict(self, name):
+        """ user_dictの枠を作る """
+        type_input = None
+        type_output = list()
+        self.user_dict[name] = (type_input, type_output)
+
     def set_addr(self, name, type_input=None, type_output=[]):
         """ addrを登録する """
         tmp_inout = self.user_dict[name]
@@ -25,22 +31,16 @@ class User_Addresses:
 
     def get_addr(self, name, put_type=None):
         """ addrを返す """
-        if (put_type is None):
-            pass
-        elif (put_type == "input"):
-            self.put_type_in(name)
-            pass
-        elif (put_type == "output"):
-            self.put_type_out(name)
-            pass
-        pass
+        tmp_inout = self.user_dict[name]
+        if (put_type is None):  # 両方
+            return tmo_inout
+        tmp_in, tmp_outs = tmp_inout
+        if (put_type == "input"):  # 入力appのaddrを返す
+            return tmp_in
+        if (put_type == "output"):  # 出力appのaddrを返す
+            return tmp_outs
+        return None
 
-    def put_type_in(self, name):
-        """ 入力app側の処理 """
-        pass
 
-    def put_type_out(self, name):
-        """ 出力app側の処理 """
-        pass
 
 
