df89f92354893424166e3bf220be1822910736bb
MakotoHamada <makoto.market0113@gmail.com>
2024-01-10 22:08:03 +0900

    usernameがあるかの判別、set_addr関数の完成

--- User_Addresses.py
+++ User_Addresses.py
@@ -5,24 +5,36 @@ class User_Addresses:
         self.user_dict = dict()
         pass
 
-    def set_addr(self, name, client_input, client_output):
-        pass
+    def name_exists(self, name):
+        if (name in self.user_dict):
+            return True
+        return False
+
+    def set_addr(self, name, type_input=None, type_output=[]):
+        tmp_inout = self.user_dict[name]
+        tmp_in, tmp_outs = tmp_inout
+        if (type_input is not None):
+            self.user_dict[name] = (type_input, tmp_outs)
+        if (type(type_output) is type(list())):
+            tmp_outs = tmp_tuple[1]
+            tmp_outs += type_output
+            self.user_dict[name] = (tmp_in, tmp_outs)
 
     def get_addr(self, name, put_type=None):
-        in_addr = None
-        out_addrs = None
         if (put_type is None):
             pass
         elif (put_type == "input"):
+            self.put_type_in(name)
             pass
         elif (put_type == "output"):
+            self.put_type_out(name)
             pass
         pass
 
-    def type_input(self, name):
+    def put_type_in(self, name):
         pass
 
-    def type_output(self, name):
+    def put_type_out(self, name):
         pass
 
 
