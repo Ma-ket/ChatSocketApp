ad601d4f02d265d0de2b9a882a8b4db1c67049e8
Da-hama <51152729+Ma-ket@users.noreply.github.com>
2024-01-16 18:12:07 +0900

    Client_Output.py を作成 #3

---
+++ Client_Output.py
@@ -0,0 +1,16 @@
+import socket
+import json
+from Client import Client
+
+class Client_Output(Client):
+    def __init__(self):
+        pass
+    
+    def run(self):
+        pass
+        
+
+if __name__ == "__main__":
+    client = Client_Output()
+    client.run()
+    
