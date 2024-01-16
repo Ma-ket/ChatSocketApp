fc43e8c127b9a24a42f0968acd165fb127c1380c
Da-hama <51152729+Ma-ket@users.noreply.github.com>
2024-01-17 08:26:28 +0900

    add request_username() #2

--- Client_Input.py
+++ Client_Input.py
@@ -1,12 +1,24 @@
 import sys
 import json
 from Client import Client
+from Packet import Packet
 
 class Client_Input(Client):
+    SERVER_ADDR = ("127.0.0.1", 8090)
+    
     def __init__(self):
-        pass
+        """ コンストラクタ """
+        super().__init__()
+        self.pckt = Packet("client", "input")
 
     def run(self):
+        """ 実行 """
+        name = super().input_username()
+        print(name)
+        pass
+        
+    def request_username(self, name):
+        """ サーバに名前の重複があるか確認する """
         pass
 
 
