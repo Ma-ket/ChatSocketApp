f0b0ded239d3f81916b7f75553752a938c8cd919
MakotoHamada <makoto.market0113@gmail.com>
2024-01-17 21:42:10 +0900

    registration_request()を追加 #3

--- Client_Output.py
+++ Client_Output.py
@@ -1,16 +1,27 @@
-import socket
-import json
 from Client import Client
+from Packet import Packet
 
 class Client_Output(Client):
+    SERVER_ADDR = ("127.0.0.1", 8090)
+
     def __init__(self):
-        pass
-    
+        """ コンストラクタ """
+        super().__init__()
+        self.pckt = Packet("client", "output")
+
     def run(self):
+        """ 実行 """
+        name = super().input_username()
+        self.registration_request(name)
         pass
-        
+
+    def registration_request(self, name):
+        """ 登録の要求、認証 """
+        data = super().create_packet(name)
+        super().send_packet(data, SERVER_ADDR)
+        print("#### packetの送信完了 ####")
+
 
 if __name__ == "__main__":
     client = Client_Output()
     client.run()
-    
