e520cc9e5872f224167ee01013da2c85d1bfc3de
MakotoHamada <makoto.market0113@gmail.com>
2024-01-17 12:29:30 +0900

    add response_username() #2

--- Client_Input.py
+++ Client_Input.py
@@ -5,7 +5,7 @@ from Packet import Packet
 
 class Client_Input(Client):
     SERVER_ADDR = ("127.0.0.1", 8090)
-    
+
     def __init__(self):
         """ コンストラクタ """
         super().__init__()
@@ -15,12 +15,28 @@ class Client_Input(Client):
         """ 実行 """
         name = super().input_username()
         print(name)
-        pass
-        
-    def request_username(self, name):
-        """ サーバに名前の重複があるか確認する """
+
+        self.request_username(name)
+        self.response_username()
         pass
 
+    def request_username(self, name):
+        """ サーバに名前の重複があるか確認する """
+        data = super().create_packet(name)
+        super().send_packet(data, SERVER_ADDR)
+
+    def response_username(self):
+        while True:
+            data, addr = super().recieve_packet()
+            if (data["app"] != "server"):
+                pass
+            else:
+                if (data["registered"] == False):
+                    comment = data["comment"]
+                    super().close(f"error: {comment}")
+                else:
+                    print("recieved the packet successfully from server!")
+                    return 0
 
 
 if __name__ == "__main__":
