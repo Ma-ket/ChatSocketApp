91864a300b2cd7eceb1eb4dd49b483d8cc72ddf6
MakotoHamada <makoto.market0113@gmail.com>
2024-01-18 17:09:07 +0900

    完成 #3

--- Client_Output.py
+++ Client_Output.py
@@ -8,11 +8,14 @@ class Client_Output(Client):
         """ コンストラクタ """
         super().__init__()
         self.pckt = Packet("client", "output")
+        self.my_username = None
 
     def run(self):
         """ 実行 """
         name = super().input_username()
         self.registration_request(name)
+        data, addr = self.wait_recieving_packet()
+        self.my_username = name
         pass
 
     def registration_request(self, name):
@@ -20,6 +23,32 @@ class Client_Output(Client):
         data = super().create_packet(name)
         super().send_packet(data, SERVER_ADDR)
         print("#### packetの送信完了 ####")
+
+    def wait_recieving_packet(self):
+        """ chatの内容を受信するまで待機する """
+        while True:
+            data, addr = super().recieve_packet()
+            self.analyze(data, addr)
+
+    def analyze(self, data, addr):
+        """ 使うかまだわからない """
+        if (data["app"] != "server"):
+            pass
+        else:
+            comment = data["comment"]
+            if (comment in ["end", "logout"]):
+                super().close("end of this program")
+            elif (comment is None):
+                pass
+            else:
+                name = data["username"]
+                self.chat(name, comment)
+
+    def chat(self, name, comment):
+        if (name == self.my_username):
+            print(f"me:{name}: {comment}")
+        else:
+            print(f"notme:{name}: {comment}")
 
 
 if __name__ == "__main__":
